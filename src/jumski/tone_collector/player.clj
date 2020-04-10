180e85c7619df4acd32a5194cb50e0ece94a5bfe
Wojtek Majewski <jumski@gmail.com>
2020-04-10 12:58:35 +0200

    Simplify

--- src/jumski/tone_collector/player.clj
+++ src/jumski/tone_collector/player.clj
@@ -3,7 +3,7 @@
            [javafx.util Duration]))
 
 (defn play-file [file]
-  (let [media (-> file (.toURI) (.toString) (Media.))
+  (let [media (-> file .toURI .toString Media.)
         start (Duration/seconds 0)
         stop (Duration/seconds 2)
         player (doto
