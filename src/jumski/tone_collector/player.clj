ad1b29af30efd4175fe1c5757b0c413131fb0c2a
Wojtek Majewski <jumski@gmail.com>
2020-04-16 16:15:58 +0200

    Various fixes to sound player:
    - reuse existing player, if playing same file
    - make sure player is stopped when reusing player
    - create new player if file changed

--- src/jumski/tone_collector/player.clj
+++ src/jumski/tone_collector/player.clj
@@ -3,24 +3,35 @@
            [javafx.util Duration]))
 
 (def *player (atom nil))
+(def *last-file (atom nil))
+
 (def start (Duration/seconds 0))
 (def stop (Duration/seconds 2))
 
 (defn replace-player!
   "Stops previous player if it is present,
   resets it to a new instance and returns it."
-  [media]
+  [file]
   (do
     (if-let [player @*player]
       (.stop player))
-    (let [player (doto
+    (let [media (-> file .toURI .toString Media.)
+          player (doto
                    (MediaPlayer. media)
                    (.setStartTime start)
                    (.setStopTime stop))]
+      (reset! *last-file file)
       (reset! *player player)
       player)))
 
+(defn get-player!
+  "If playing same file as previously, just deref *player, stop and return it.
+  If playing new file, create new player, reset *player and return it."
+  [file]
+  (let [last-file @*last-file]
+    (if (and (= file last-file) @*player)
+      (doto @*player .stop)
+      (replace-player! file))))
+
 (defn play-file [file]
-  (let [media (-> file .toURI .toString Media.)
-        player (replace-player! media)]
-    (.play player)))
+  (-> file get-player! .play))
