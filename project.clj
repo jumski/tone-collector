bcfb743dcaae6fde657798a4257eb27db8039cfc
Wojtek Majewski <jumski@gmail.com>
2020-04-16 17:13:33 +0200

    Add lein-native-image

--- project.clj
+++ project.clj
@@ -3,15 +3,15 @@
   :url "https://github.com/jumski/tone-collector"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :plugins [[io.aviso/pretty "0.1.37"]]
-  :middleware [io.aviso.lein-pretty/inject]
+  :plugins [[io.taylorwood/lein-native-image "0.3.1"]]
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [cljfx "1.6.7"]
-                 [overtone/midi-clj "0.5.0"]
-                 #_[slingshot "0.12.2"]
-                 #_[io.aviso/pretty "0.1.37"]
-                 #_[expound "0.8.4"]]
+                 [overtone/midi-clj "0.5.0"]]
   :main ^:skip-aot jumski.tone-collector.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
-                       :injections [(javafx.application.Platform/exit)]}})
+                       :native-image {:name "tone-collector"
+                                      :jvm-opts ["-Dclojure.compiler.direct-linking=true"]
+                                      :opts ["--verbose"
+                                             "--no-server"
+                                             "-H:+ReportUnsupportedElementsAtRuntime"]}}})
